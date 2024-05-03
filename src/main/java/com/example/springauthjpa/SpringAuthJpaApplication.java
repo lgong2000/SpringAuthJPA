02c217c6543b1a2253733e83caf20fb9f7615b45
lgong2000 <lgong2000@gmail.com>
2024-05-03 15:07:15 -0500

    Initial Commit

---
+++ src/main/java/com/example/springauthjpa/SpringAuthJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springauthjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthJpaApplication.class, args);
+    }
+
+}
